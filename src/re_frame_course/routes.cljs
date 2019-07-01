ee30d8f97a3ef1331e90119b20f0416ebee5b354
Saad Guessous <guessous_saad@hotmail.com>
2019-07-01 06:44:32 +0000

    added routes for new components

--- src/re_frame_course/routes.cljs
+++ src/re_frame_course/routes.cljs
@@ -6,7 +6,11 @@
             [goog.history.EventType :as EventType]
             [re-frame-course.state :refer [app-state]]
             [re-frame-course.components.forms :refer [components-panel]]
-            [re-frame-course.components.hiccup-panel :refer [hiccup-panel]]))
+            [re-frame-course.components.hiccup-panel :refer [hiccup-panel]]
+            [re-frame-course.components.atoms :refer [atoms-panel]]
+            [re-frame-course.components.events :refer [events-panel]]
+            [re-frame-course.components.subscriptions :refer [subscriptions-panel]]
+            ))
 
 (defn hook-browser-navigation! []
   (doto (Html5History.)
@@ -28,6 +32,15 @@
   (defroute "/hiccup-panel" []
     (swap! app-state assoc :page :hiccup-panel))
 
+  (defroute "/atoms-panel" []
+    (swap! app-state assoc :page :atoms-panel))
+
+  (defroute "/events-panel" []
+    (swap! app-state assoc :page :events-panel))
+
+  (defroute "/subscriptions-panel" []
+    (swap! app-state assoc :page :subscriptions-panel))
+
   (hook-browser-navigation!))
 
 (defn home []
@@ -35,13 +48,25 @@
    [:h1 "Hiccup"]
    [:hr]
    [:p [:a {:href "#/components"} "Components"]]
-   [:p [:a {:href "#/hiccup-panel"} "Hiccup-panel"]]])
+   [:p [:a {:href "#/hiccup-panel"} "Hiccup"]]
+   [:p [:a {:href "#/atoms-panel"} "Atoms"]]
+   [:p [:a {:href "#/events-panel"} "Events"]]
+   [:p [:a {:href "#/subscriptions-panel"} "Subscriptions"]]])
 
 (defn components []
   [components-panel])
 
 (defn hiccup-component []
   [hiccup-panel])
+
+(defn atoms-component []
+  [atoms-panel])
+
+(defn events-component []
+  [events-panel])
+
+(defn subscriptions-component []
+  [subscriptions-panel])
 
 (defmulti current-page #(@app-state :page))
 (defmethod current-page :home []
@@ -50,5 +75,11 @@
   [components])
 (defmethod current-page :hiccup-panel []
   [hiccup-component])
+(defmethod current-page :atoms-panel []
+  [atoms-component])
+(defmethod current-page :events-panel []
+  [events-component])
+(defmethod current-page :subscriptions-panel []
+  [subscriptions-component])
 (defmethod current-page :default []
   [:div "default"])
